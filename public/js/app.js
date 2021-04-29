7a8a5e9f56d7a9a0be5bfb12dc520b974ba72de7
cheisentrout <cheisentrout@gmail.com>
2021-04-29 10:39:25 -0400

    Toggle class function working

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,30 @@
 console.log('Static JS hooked up');
 
 const detailsSection = document.getElementById('details')
-const detailsBtn = document.querySelector('#view-details')
+const detailsBtn = document.getElementById('view-details')
+const portSection = document.getElementById('portfolio')
+const portfolioBtn = document.getElementById('view-portfolio')
+
 
 // const show = (elem) => {
 //   console.log('show ran');
 //   elem.style.display = 'block'
 // }
 
-// detailsBtn.addEventListener('click', () => {
-//   // if (detailsSection.style.display === 'none') {
-//     detailsSection.style.display = "block"
-//     detailsBtn.innerText = "Hide Details"
-//   // }
-//   // else {
-//   //   detailsSection.style.display = 'none'
-//   //   detailsBtn.innerText = "View Details"
-//   // }
-// })
-
 detailsBtn.addEventListener('click', () => {
   detailsSection.classList.toggle('hidden')
   if (!detailsSection.classList.contains('hidden')) {
     detailsBtn.innerText = 'Hide Details'
+  } else {
+    detailsBtn.innerText = 'View Details'
+  }
+})
+
+portfolioBtn.addEventListener('click', () => {
+  portSection.classList.toggle('hidden')
+  if (!portSection.classList.contains('hidden')) {
+    portfolioBtn.innerText = "Hide Portfolio"
+  } else {
+    portfolioBtn.innerText = "View Portfolio"
   }
 })
