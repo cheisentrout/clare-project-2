ab373995889fe88261d40aed345c6a7288219558
cheisentrout <cheisentrout@gmail.com>
2021-04-30 11:35:27 -0400

    Setting up users controller and landing page routes

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,8 +4,14 @@ const Student = require('../models/students.js')
 const Portfolio = require('../models/portfolio.js')
 const User = require('../models/users.js')
 
-user.get('/user', (req, res) => {
-  res.send('I am the user route, hear me roar.')
+user.get('/user/new', (req, res) => {
+  res.render(
+    'users/new.ejs'
+  )
+})
+
+user.get('/user/login', (req, res) => {
+  res.send("Here is the log in page.")
 })
 
 module.exports = user
